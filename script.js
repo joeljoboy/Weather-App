2671e19793c6fe14d18c1c5141d643e6dbd70971
Joel Joboy <joeljoboyjoseph75@gmail.com>
2023-07-13 01:00:59 +0530

    Added video background

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// ////////////////////video background responsiveness///////////////////////
+window.addEventListener('resize', function() {
+    var videoContainer = document.querySelector('.video-container');
+    videoContainer.style.height = window.innerHeight + 'px';
+  });
+  
