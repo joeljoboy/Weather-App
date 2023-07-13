3f7fbdcb15041270e4dbdc7952d1a279ad18945c
Joel Joboy <joeljoboyjoseph75@gmail.com>
2023-07-13 17:29:55 +0530

    Waiting for api to get active

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const apiKey="6ab5b21044d19178f17bb05f8218e6c7";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
+async function checkWeather()
+{
+  const response = await fetch(apiUrl+`&appid=${apiKey}`);
+  var data= await response.json();
+
+  console.log(data);
+}
+checkWeather();
 // ////////////////////video background responsiveness///////////////////////
 window.addEventListener('resize', function() {
     var videoContainer = document.querySelector('.video-container');
